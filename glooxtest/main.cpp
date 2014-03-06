3a89265eb161d9fcedc220f2b9f1d162c6da21ec
Tux <tux@netz39.de>
2014-03-06 20:00:36 +0100

    Use config files to store XMPP settings.

--- glooxtest/main.cpp
+++ glooxtest/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+
 
 #include <gloox/jid.h>
 #include <gloox/client.h>
@@ -6,6 +8,8 @@
 #include <gloox/message.h>
 #include <gloox/messagesession.h>
 #include <gloox/messagesessionhandler.h>
+
+#include <libconfig.h++>
 
 using namespace std;
 using namespace gloox;
@@ -50,21 +54,44 @@ Client* I2CController::client() {
 int main(int argc, char **argv) {
     cout << "Hello, world!" << endl;
 
+    libconfig::Config cfg;
+
+    try {
+        cfg.readFile("i2ccontrol.config");
+    } catch (const libconfig::FileIOException &fioex) {
+        cerr << "I/O error on reading config file: " << fioex.what() << endl;
+        return (-1);
+    } catch(const libconfig::ParseException &pex) {
+        cerr << "Parse error at " << pex.getFile() << ":" << pex.getLine()
+             << " - " << pex.getError() << endl;
+        return(-1);
+    }
+
+    Client* client = 0;
+    try {
+        string jid = cfg.lookup("xmpp.jid");
+        string password = cfg.lookup("xmpp.password");
+
+        cout << "Got jid " << jid << endl;
+
+        JID ui_jid(jid);
+        client = new Client(ui_jid, password);
+    } catch (const libconfig::SettingNotFoundException &snfex) {
+        cerr << "Setting not found: " << snfex.getPath() << endl;
+    }
 
 
-    JID ui_jid("i2c@platon");
-    Client* client = new Client(ui_jid, "i2c");
+    if (client) {
+        I2CController* i2c = new I2CController(client);
 
-    I2CController* i2c = new I2CController(client);
-
-    if (!client->connect(true))
-        cerr << "could not connect!" << endl;
+        if (!client->connect(true))
+            cerr << "could not connect!" << endl;
 
 
 
 
-    delete client;
-
+        delete client;
+    }
 
     return 0;
 }
